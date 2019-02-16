8f87dad2eec92eb03893387eb45c934cda99a61c
yromanchuk <anybis2007@gmail.com>
2019-02-16 23:16:46 +0200

    Started learning git

--- src/main/java/notebook/AccessPoint.java
+++ src/main/java/notebook/AccessPoint.java
@@ -22,8 +22,8 @@ public class AccessPoint {
     }
 
     public void startProgram() {
-
-        List<Validator> validators = receiveValidators();
+/*
+        List<Validator> validators = receiveValidators();*/
 
     }
 
